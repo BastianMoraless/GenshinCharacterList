fe84a4253b436aa7d5eddb43e494cbb19c6f7df7
Bastian Morales <bastianma.contacto@gmail.com>
2024-11-11 18:50:36 -0300

    Validacion en Backend

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'uploads')); // Servir imágenes estáticas
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors()
   await app.listen(3000);
 }
--- back/src/personaje/dto/create-personaje.dto.ts
+++ back/src/personaje/dto/create-personaje.dto.ts
@@ -1,17 +1,37 @@
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Length} from "class-validator"
+
 export class CreatePersonajeDto {
+
+    @IsString()
+    @IsNotEmpty()
     nombre: string
 
+    @IsString()
+    @IsNotEmpty()
     titulo: string
 
+    @IsString()
+    @IsNotEmpty()
     informacionGeneral: string
 
+    @IsNumber()
+    @IsNotEmpty()
     rareza: number
 
+    @IsString()
+    @IsNotEmpty()
     elemento: string
 
+    @IsString()
+    @IsNotEmpty()
     constelacion: string
 
+    @IsString()
+    @IsNotEmpty()
+    @Length(5, 5)
     cumpleanos: string
 
+    @IsString()
+    @IsOptional()
     imagenUrl?: string;
 }
